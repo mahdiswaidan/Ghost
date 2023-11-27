384f0ee94898a5f6e3821293dea7508602c3a262
kubo <mohammed.swidan@naseej.com>
2023-11-27 07:30:47 +0000

    yaml

--- apps/admin-x-framework/src/index.ts
+++
@@ -1,6 +0,0 @@
-export {default as FrameworkProvider, useFramework} from './providers/FrameworkProvider';
-export type {FrameworkContextType, FrameworkProviderProps, TopLevelFrameworkProps} from './providers/FrameworkProvider';
-
-export {useQueryClient} from '@tanstack/react-query';
-export type {InfiniteData} from '@tanstack/react-query';
-
--- apps/admin-x-framework/src/routing.ts
+++
@@ -1,3 +0,0 @@
-export {default as RoutingProvider, useRouteChangeCallback, useRouting} from './providers/RoutingProvider';
-export type {ExternalLink, InternalLink, ModalComponent, RoutingModalProps} from './providers/RoutingProvider';
-
